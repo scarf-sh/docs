c0c89fe34b3bd9f2921c2fd93eef2de49f51d904
aviaviavi <avipress@gmail.com>
2023-03-06 17:36:38 -0800

    remove pre-commit

--- devenv.nix
+++ devenv.nix
@@ -1,10 +1,8 @@
 { pkgs, ... }:
 
 {
-  # https://devenv.sh/basics/
   env.GREET = "devenv";
 
-  # https://devenv.sh/packages/
   packages = [
     pkgs.git
   ];
@@ -13,14 +11,8 @@
     pip install -r requirements.txt
   '';
 
-  # https://devenv.sh/languages/
   languages.python.enable = true;
   languages.python.venv.enable = true;
 
-  # https://devenv.sh/pre-commit-hooks/
-  pre-commit.hooks.markdownlint.enable = true;
-
-  # https://devenv.sh/processes/
-  # processes.ping.exec = "ping example.com";
   processes.serve.exec = "mkdocs serve";
 }
