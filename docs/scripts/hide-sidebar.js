31d546e935a142d182e7729cc6ec354f0312143c
Fabio Luz <superfabio@Fabios-MacBook-Pro.local>
2023-07-20 11:29:22 -0300

    Use new API docs

--- docs/scripts/hide-sidebar.js
+++
@@ -1,6 +0,0 @@
-window.onload = function() {
-  const sideBar = window.document.getElementsByClassName('md-sidebar--secondary').item(0);
-  if (sideBar) {
-    sideBar.style.display = 'none';
-  }
-};
